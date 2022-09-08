f136d8e50c384ac3bb71f94ebd54bbe0fb1d9f7f
Brian Bassey <brianbassey37@gmail.com>
2022-09-07 22:52:27 -0700

    shared

--- quit_shell.c
+++ quit_shell.c
@@ -1,18 +1,18 @@
 #include "simple_shell.h"
 
 /**
- * exit_shell - exits the shell
- *
- * @datash: data relevant (status and args)
- * Return: 0 on success.
- */
+* exit_shell - exits the shell
+*
+* @datash: data relevant (status and args)
+* Return: 0 on success.
+*/
 int exit_shell(data_shell *datash)
 {
 	unsigned int ustatus;
 	int is_digit;
 	int str_len;
 	int big_number;
-
+s
 	if (datash->args[1] != NULL)
 	{
 		ustatus = _atoi(datash->args[1]);
